9ef5013edbb4fd7014c1666f153a3c040b6a01cc
Amir Allahvern <53536617+amir4141-creator@users.noreply.github.com>
2021-02-03 00:04:01 +0330

    controller completed . but it should be validate

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,8 @@ import models.*;
 import view.*;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Controller {
@@ -13,19 +15,20 @@ public class Controller {
             public SystemData data;
 
             @Override
-            protected boolean signUpActionLoginPanel(String username, String password) {  // sign up as publisher or customer??
+            protected boolean signUpActionLoginPanel(String username, String password) {
+                //todo: sign up as publisher or customer??
                 return false;
             }
 
             @Override
             protected boolean loginActionLoginPanel(String username, String password) {
-                return data.userExist(username,password);
+                return data.userExist(username, password);
             }
 
             @Override
             protected int postActionPostPanel(String title, String text, int price, boolean isDownloadable, boolean isPublic) {
                 Content content = new Content(title, text, price, isDownloadable);
-                data.publishContent((Publisher)data.getOnlineCustomer(), content);
+                data.publishContent((Publisher) data.getOnlineCustomer(), content);
                 return data.getLastContentIndex();
 //                if(isPublic) {
 //                    ui.getPublicPage().addContentBox(ContentBox(content));
@@ -43,7 +46,7 @@ public class Controller {
 
             @Override
             protected boolean changeNameActionProfilePanel(String nawName) {
-                if(data.userExist(nawName))
+                if (data.userExist(nawName))
                     return false;
                 data.getOnlineCustomer().changeName(nawName);
                 return true;
@@ -51,9 +54,10 @@ public class Controller {
 
             @Override
             protected boolean addMoneyActionProfilePanel(int changeMoneyAmount) {
-                if( changeMoneyAmount<0 )
+                if (changeMoneyAmount < 0)
                     return false;
                 data.getOnlineCustomer().addMoney(changeMoneyAmount);
+                return false;
             }
 
             @Override
@@ -68,7 +72,7 @@ public class Controller {
 
             @Override
             protected Role getRoleProfilePanel() {
-                if ( data.getOnlineCustomer() instanceof Publisher )
+                if (data.getOnlineCustomer() instanceof Publisher)
                     return Role.PUBLISHER;
                 return Role.CUSTOMER;
             }
@@ -76,7 +80,7 @@ public class Controller {
             @Override
             protected String[] getPublishersNameArraySubscribePanel() {
                 String[] publishersName = new String[data.getPublishers().size()];
-                for(int i=0; i<data.getPublishers().size(); i++){
+                for (int i = 0; i < data.getPublishers().size(); i++) {
                     publishersName[i] = data.getPublishers().get(i).getUserName();
                 }
                 return publishersName;
@@ -84,7 +88,7 @@ public class Controller {
 
             @Override
             protected void setSubscribedPublishersSubscribePanel(ArrayList<String> selectedPublisherNames) {
-
+                //todo: amir : I did not understand it
             }
 
             @Override
@@ -99,47 +103,63 @@ public class Controller {
 
             @Override
             protected int getLikeNumber(int index) {
-                return 0;
+                return data.getContents().get(index).getLikes();
             }
 
             @Override
             protected boolean isDownloadable(int index) {
-                return false;
+                return data.getContents().get(index).getDownloadable();
             }
 
             @Override
             protected ArrayList<Integer> getMagazineIndexList() {
-                return null;
+                //TODO : amir :  why integer? i can send contents sizes THEN you have 0 till content size as index
+                ArrayList<Integer> indexes = new ArrayList<>();
+                for(int i = 0 ; i< data.getContents().size() ; i++){
+                    indexes.add(i);
+                }
+                return indexes;
             }
 
             @Override
             protected void logoutButtonAction() {
-
+                data.setOnline(-1);
             }
 
             @Override
             protected void downloadButtonAction(int index) {
-
+                String path = "./Downloads/" + data.getOnlineCustomer().getUserName() + "/";
+                File file = new File(path);
+                try {
+                    file.mkdir();
+                    FileWriter writer = new FileWriter( path + data.getContents().get(index).getTitle() + ".txt");
+                    writer.write(data.getContents().get(index).getData());
+                    writer.close();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
             }
 
             @Override
             protected ArrayList<String[]> getCommentByIndex(int index) {
-                return null;
+               return data.getContents().get(index).getComments();
             }
 
             @Override
             protected boolean addComment(int magazineIndex, String[] newComment) {
-                return false;
+                data.getContents().get(magazineIndex).getComments().add(newComment);
+                return true;
             }
 
             @Override
             protected void incrementLikeByIndex(int index) {
-
+                data.getContents().get(index).incrementLikes();
             }
 
             @Override
             protected void decrementLikeByIndex(int index) {
-
+                data.getContents().get(index).decrementLikes();
             }
         });
     }
